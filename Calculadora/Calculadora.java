ea200e3a424f59282a684520b50ae85f6c5e58ec
Adriano Duzanjos <adrianodz.net@hotmail.com>
2023-02-14 22:03:19 -0300

    Update Calculadora.java

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -1,43 +1,36 @@
 package projetos;
 
-import java.util.Scanner;
-
-/**
-* Desafio da Calculadora, 
-* usando somente o que foi 
-* aprendido até agora no
-* curso.
-* 
-* @author Duzanjos
-* @since 1.0
-* @see https://github.com/Duzanjos
-*/
+import javax.swing.JOptionPane;
 
 public class Calculadora {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("#------- 1º Calculadora --------#");
-		System.out.print("     Digite o 1º Número: ");
-		double n1 = scan.nextDouble();
-		System.out.print("     Digite o 2º Número: ");
-		double n2 = scan.nextDouble();
 		
-		System.out.println("#-------------------------------#");
-		System.out.println(" Informe a operação matemática, ");
-		System.out.println(" com as seguintes opções abaixo:");
-		System.out.println("           + - * / %");
-		String op = scan.next();
+		String inf01 = JOptionPane.showInputDialog(""
+				+ "---------- Calculadora ---------\n"
+				+ "     Digite o 1º Número: ");
+		int n1 = Integer.parseInt(inf01);
+		String inf02 = JOptionPane.showInputDialog(""
+				+ "---------- Calculadora ---------\n"
+				+ "     Digite o 2º Número: ");
+		int n2 = Integer.parseInt(inf02);
 		
+		String op = JOptionPane.showInputDialog(""
+				+ "Informe a operação matemática \n"
+				+ "com as seguintes opções abaixo:\n"
+				+ "           +   -   *   /   %");
 		//Lógica
-		double resultado = "+".equals(op) ? n1 + n2 : 0;
-		resultado = "-".equals(op) ? n1 - n2 : resultado;
-		resultado = "*".equals(op) ? n1 * n2 : resultado;
-		resultado = "/".equals(op) ? n1 / n2 : resultado;
-		resultado = "%".equals(op) ? n1 % n2 : resultado;
-		
-		System.out.println("#--------- :Resposta: ----------#");
-		System.out.printf("     %.2f %s %.2f = %.2f", n1, op, n2, resultado);
-		scan.close();
+		double resultado = 0;
+
+		if		(op.equals("+")) {resultado = n1 + n2;
+		}else if(op.equals("-")) {resultado = n1 - n2;
+		}else if(op.equals("*")) {resultado = n1 * n2;
+		}else if(op.equals("/")) {resultado = n1 / n2;
+		}else if(op.equals("%")) {resultado = n1 % n2;
+		}else{
+			JOptionPane.showMessageDialog(null, "Você Digitou uma opção inválida", "Erro", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		JOptionPane.showMessageDialog(null, resultado);
 	}
 }
